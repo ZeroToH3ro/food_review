22ca9fb682a76ff4d358d33564a5d1dd8060ca13
ZeroToH3ro <mtblaser2002@gmail.com>
2024-03-17 01:58:25 +0700

    setup firebase for main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_review/firebase_options.dart';
 import 'package:food_review/pages/home.dart';
 
 import 'helper/themes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+
+  );
+
   runApp(const MyApp());
 }
 
